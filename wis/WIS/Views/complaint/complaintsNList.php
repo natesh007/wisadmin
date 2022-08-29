9a1e06f51d7d72f53a045dc2c32b9d72c9a5eab0
Lakshmi-IGS <lakshmi@igreensystems.com>
2022-08-29 18:29:57 +0530

    Small change

--- wis/WIS/Views/complaint/complaintsNList.php
+++ wis/WIS/Views/complaint/complaintsNList.php
@@ -293,8 +293,8 @@
         }
         $(function() {
 
-            var start = moment().startOf('month');
-            var end = moment().endOf('month');
+            var start = moment().subtract(29, 'days');
+            var end = moment();
 
             function cb(start, end) {
                 $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
